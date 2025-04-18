799edf45f6197ffbd60995685f328f8f3569e59f
Pedro Roa <p.roac@outlook.com>
2025-04-18 00:46:38 -0400

    Update project id

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "tf_sa_key" {
   description = "Service account key for GCP"
   default     = ""
 }
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+  default     = "home-server-457102"
+}
