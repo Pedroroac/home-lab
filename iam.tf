0cc4ff48560f569065c6e36908fc79435839a46c
Pedro Roa <p.roac@outlook.com>
2025-04-18 23:58:15 -0400

    Fix iam membership

--- iam.tf
+++ iam.tf
@@ -4,10 +4,10 @@ resource "google_service_account" "truenas_backup" {
   description  = "Service account used by TrueNAS Scale to upload backups to GCP"
 }
 
-resource "google_service_account_iam_member" "truenas_backup_storage_admin" {
-  service_account_id = google_service_account.truenas_backup.name
-  role               = "roles/storage.objectAdmin"
-  member             = "serviceAccount:${google_service_account.truenas_backup.email}"
+resource "google_project_iam_member" "truenas_backup_storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.truenas_backup.email}"
 }
 
 resource "google_service_account_key" "truenas_backup_key" {
