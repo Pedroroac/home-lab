2a901ddda724652d76be401667eb48fcc5b58df4
Pedro Roa <p.roac@outlook.com>
2025-12-03 21:28:39 -0500

    storage class migration

--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -1,7 +1,19 @@
 resource "google_storage_bucket" "backup-bucket" {
   name          = "backup-bucket-${var.project_id}"
   location      = "NORTHAMERICA-NORTHEAST2"
-  force_destroy = true
+  force_destroy = false
 
   public_access_prevention = "enforced"
+
+  storage_class = "ARCHIVE"
+
+  lifecycle_rule {
+    action {
+      type = "SetStorageClass"
+      storage_class = "ARCHIVE"
+    }
+    condition {
+      matches_storage_class = ["STANDARD", "NEARLINE", "COLDLINE"]
+    }
+  }
 }
